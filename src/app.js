8b9c6c9b05734315222445bad7f55a3787b827d1
r1ddax <r1ddax00@gmail.com>
2021-05-16 21:25:44 -0300

    Playlist song system

--- src/app.js
+++ src/app.js
@@ -24,8 +24,8 @@ client.on('ready', () => {
 
 // Clear chat
 let getIdWhoExecuted;
-client.on('message', function(msg) {
 
+client.on('message', function(msg) {
   // Clear system
   if (msg.content === prefix + "clear") {
     getIdWhoExecuted = msg.author.id;
@@ -43,7 +43,10 @@ client.on('message', function(msg) {
 });
 
 // Voice Channel
-let music; // Song vars
+let music;
+let status = "disconnected";
+let song_list = [];
+let nextOne;
 
 client.on('message', async msg => {
 	// Join the same voice channel of the author of the message
@@ -51,6 +54,7 @@ client.on('message', async msg => {
     if (msg.member.voice.channel) {
       msg.reply("I'm joining the voice channel ..");
       const connection = await msg.member.voice.channel.join();
+      status = "connected";
     }else{
       msg.reply("You need to join voice channel");
     }
@@ -64,6 +68,7 @@ if (msg.content === prefix + "leave") {
       if (voiceChannelM == voiceChannelC){
         msg.reply("I'm leaving the voice channel ..");
         const connection = await msg.member.voice.channel.leave();
+        status = "disconnected"
       }else{
         msg.reply("I'm not on this channel");
       }
@@ -72,34 +77,91 @@ if (msg.content === prefix + "leave") {
     }
 }
 
-if ((msg.content.startsWith(prefix + 'play'))) {
-  let _msg = msg.content + '';
-  let splitedUrl = '';
+  // Playing song
+  if (status === "connected" || status === "nomusic" || status === "waiting_song")
+  {
+    if ((msg.content.startsWith(prefix + 'add'))) {
+      if (song_list.length === 0) { status = "connected" };
 
-  // Youtube mode 1 and mode 2
-  if ((_msg.indexOf('?v=') > -1)) { 
-      splitedUrl = _msg.split('?v=');
-  }else if ((_msg.indexOf('youtu.be/') > -1)) {
-      splitedUrl = _msg.split('youtu.be/');      
-  }else{
-      msg.reply("Please try to use a youtube url, example: www.youtube.com/watch?v=bcQwIxRcaYs");
-      return;
+      let _msg = msg.content + '';
+      let splitedUrl = '';
+
+      // Youtube mode 1 and mode 2
+      if ((_msg.indexOf('?v=') > -1)) { 
+          splitedUrl = _msg.split('?v=');
+          msg.reply("Song added to playlist !");
+      }else if ((_msg.indexOf('youtu.be/') > -1)) {
+          splitedUrl = _msg.split('youtu.be/');
+          msg.reply("Song added to playlist !");
+      }else{
+          msg.reply("Please try to use a youtube url, example: www.youtube.com/watch?v=bcQwIxRcaYs");
+          return;
+      }
+      splitedUrl.shift();
+      song_list.push(splitedUrl[0]);
+      
+    }
   }
 
-  msg.reply(`Playing music now! 🎵`);
-  const connection = await msg.member.voice.channel.join();
-  music = connection.play(ytdl('https://www.youtube.com/watch?v=' + splitedUrl[1], { filter: 'audioonly' }));
-  // music.setVolume(0.5);
-}
+  if ((status === "nomusic" || status === "waiting_song") && song_list.length === 0)
+  {
+    if (((msg.content === prefix + 'play') && song_list.length === 0)) {
+      status = "waiting_song";
+    }
 
-if (msg.content == (prefix + 'stop')) {
-  // if(!client.music.isPlaying()) {
-  //   msg.reply('Do not have any song playing now');
-  // }else{
-    music.pause();
-  // }
-}
+    if (status === "waiting_song"){
+    status = "nomusic";
+    console.log(song_list);
+    
+      setTimeout(function(){
+        msg.reply(`Do not have any songs, try ${prefix}add youtube_url`);
+      }, 1000);
+    }
+  }
 
+  // Play first song
+  if (status === "connected" || status === "play_next") {
+    const connection = await msg.member.voice.channel.join();
+    music = connection.play(ytdl('' + song_list[0], { filter: 'audioonly' }));
+
+    if ( (((msg.content === prefix + 'play')) && song_list.length > 0)){
+      music = connection.play(ytdl('https://www.youtube.com/watch?v=' + song_list[0], { filter: 'audioonly' }));
+      msg.reply(`Playing music now! 🎵`);
+      nextOne = false;
+      status = "connected";
+    }
+
+    music.on('finish', () => { // 'Finished playing!'
+      console.log('Song finished');
+      status = "play_next";
+      console.log(status);
+    
+      if (status == "play_next") {
+        song_list.shift();
+        if (song_list.length === 0) {
+          console.log('finished songs, waiting');
+          msg.reply('Finished songs');
+          status = "waiting_song";
+        }else{
+          console.log('next song');
+          msg.reply('Next song');
+          nextOne = true;
+          status = "connected";
+        }
+      }
+    
+    });
+  
+  }
+
+    if (msg.content == (prefix + 'stop')) {
+      // if(!client.music.isPlaying()) {
+      //   msg.reply('Do not have any song playing now');
+      // }else{
+        music.pause();
+      // }
+    }
+  
 });
 
 // Functions
